87d6839ea68cba4c306c6bd307d073008688cac5
srijabal <srijaabal@gamil.com>
2023-12-28 13:40:09 +0530

    relaxation page,ui

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API || "AIzaSyAwWyWNr65atxXbwNC1cVGr7d8ViMseN8Y" ,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "study-ace-6f433.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "study-ace-6f433",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "study-ace-6f433.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "172800211090",
-  appId: process.env.FIREBASE_APP_ID || "1:172800211090:web:4f91a1e35480f2b087ce4b",
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-01FT2PB891"
+  apiKey: process.env.FIREBASE_API  ,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.FIREBASE_APP_ID ,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID 
 };
 
 
