891d49232f36e326c02c07c74e3b575fb86f820f
srijabal <srijaabal@gmail.com>
2024-01-11 13:10:32 +0530

    music api,youtube playlists,quote generator

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./loginregister.css"
-/*import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase";*/
+
 import axios from 'axios';
 import { Link, useNavigate} from 'react-router-dom';
 
 
 function RegistrationForm() {
-  /*const [name, setName] = useState("");*/
-  /*const [email, setEmail] = useState("");*/
+  
   const [isRegistering, setIsRegistering] = useState(false);
-  /*const [password, setPassword] = useState("");*/
+ 
   const [values, setValues] = useState({
     name: '',
     email: '',
@@ -21,50 +19,24 @@ function RegistrationForm() {
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
   const handleNameChange = (event) => {
-    /*setName(event.target.value);*/
+    
   };
 
-  /*const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-  */
+ 
   const handleRegistrationSubmit = async (event) => {
     event.preventDefault();
     try {
       await axios.post('http://localhost:3001/register', values);
-      navigate('/Login'); // Navigate to login page immediately after successful signup
+      navigate('/Login'); 
     } catch (error) {
       console.error("An error occurred:", error);
       alert("An error occurred. Please try again.");
     }
   }
    
-    /*try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-      console.log(user);
-      alert("You have successfully registered");
-      setEmail("");
-      setPassword("");
-    } catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(errorCode, errorMessage);
-      alert("Something went wrong. please Try again: " + errorMessage);
-    }
-  };
-  */
+    
   useEffect(() => {
-    /*setEmail("");
-    setPassword("");*/
+    
   }, [isRegistering]);
 
   return (
@@ -76,7 +48,7 @@ function RegistrationForm() {
           type="text"
          
           
-          /*onChange={handleNameChange}*/
+          
           placeholder="Enter your name"
           value={name}
           onChange={e => setValues({...values, name: e.target.value})}
@@ -86,8 +58,7 @@ function RegistrationForm() {
         <label>Email:</label>
         <input
           type="email"
-          /*value={email}
-          onChange={handleEmailChange}*/
+          
           placeholder="Enter your email"
           value={email}
           onChange={e => setValues({...values, email: e.target.value})}
@@ -96,8 +67,7 @@ function RegistrationForm() {
         <label>Password:</label>
         <input
           type="password"
-          /*value={password}
-          onChange={handlePasswordChange}*/
+          
           placeholder="Enter your password"
           value={password}
           onChange={e => setValues({...values, password: e.target.value})}
